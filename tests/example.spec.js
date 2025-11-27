3e97454185a31a6b517b65f74d9ca73d6999592b
Varga Ákos <149047059+vargacska1@users.noreply.github.com>
2025-11-27 13:32:33 +0100

    Refactor CI/CD pipeline: remove Docker Compose step and update Playwright base URL to include port

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -15,5 +15,7 @@ test('Kudos Wall E2E', async ({ page }) => {
   await page.getByRole('button', { name: 'Küldés' }).click();
 
   await expect(page.getByText(`Mert teszt!_${time}`)).toBeVisible();
-  await expect(page.getByText(`To: Tesztnek_${time}`)).toBeVisible();
+  
+  const cardHeader = page.locator('.card-header').filter({ hasText: `To: Tesztnek_${time}` });
+  await expect(cardHeader).toBeVisible();
 });
