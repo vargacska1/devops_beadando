2c22f3d10361cd53e991d1e1b63f4b8c1cdbb944
Varga Ákos <149047059+vargacska1@users.noreply.github.com>
2025-11-28 15:49:35 +0100

    Update API proxy target in Vite config to use production endpoint

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://api.lid75l.me',
         changeOrigin: true,
         secure: false,
         }
